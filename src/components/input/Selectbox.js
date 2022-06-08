c54b100a40cd1c94c385810f575129f546b91e38
Alexander Kremeshnoi <alexander.kremeshnoi@gmail.com>
2022-06-08 08:09:03 +0300

    feat: improved select box with required option

--- src/components/input/Selectbox.js
+++ src/components/input/Selectbox.js
@@ -3,7 +3,7 @@ import React, {useState, useCallback, useEffect} from "react";
 import {generate} from "shortid";
 import {ChevronDownIcon} from "@heroicons/react/outline";
 
-const Selectbox = ({value, setValue, selectedValue, title, twSelect}) => {
+const Selectbox = ({value, setValue, selectedValue, title, isRequired, twSelect, twRequired}) => {
   const onValueChangeHandler = useCallback((event) => {
     setValue(event.currentTarget.value);
   }, [selectedValue]);
@@ -14,8 +14,11 @@ const Selectbox = ({value, setValue, selectedValue, title, twSelect}) => {
 
   return (
     <div className={`flex flex-col ${twSelect}`}>
-      <label className={"text-gray-600"}>{title}</label>
-      <div className={`relative mt-1`}>
+      <label className={"text-gray-800 mb-2"}>
+        {title}
+        {isRequired && <span className={`text-red-500 ${twRequired}`}> * </span>}
+      </label>
+      <div className={`relative`}>
         <ChevronDownIcon className={"absolute z-10 right-2 top-2 text-gray-300"} height={20} width={20} />
         <select
           className={`p-1 border border-gray-300 rounded-sm w-full appearance-none relative focus:outline-none text-gray-600`}
