4e7bfc0134844b9f1c90025a7e072107b77dcff0
Alexander Kremeshnoi <alexander.kremeshnoi@gmail.com>
2022-06-15 12:17:15 +0300

    chore: cleaning up

--- src/feature/task/api/newTaskApi.js
+++ src/feature/task/api/newTaskApi.js
@@ -1,19 +1,16 @@
-import {useContext} from "react";
-import AuthContext from "../../../context/authContext";
 import env from "../../../env";
 import Async from "crocks/Async";
-import maybeToAsync from "crocks/Async/maybeToAsync";
 import {fetchGql} from "@s-e/frontend/fetch";
 import {caseMap} from "@s-e/frontend/flow-control";
 import {lengthGt} from "../../../util/pred";
-import {getProp, tap, pipe, chain, safe, option, map, isString, getPath, hasProps} from "crocks";
+import {getProp, tap, pipe, chain, safe, option, map, isString, getPath} from "crocks";
 import {t} from "../../../i18n";
 
 const {Rejected} = Async;
 
 const createEventQuery = `
-  mutation instertEvent($name: String!, $desctiption: String!, $status: String!, $crewID: uuid!, $objectID: Int!) {
-    insert_events(objects: {name: $name, description: $desctiption, status: $status, crew_id: $crewID, object_id: $objectID}) {
+  mutation insertEvent($name: String!, $description: String!, $status: String!, $crewID: uuid!, $objectID: Int!) {
+    insert_events(objects: {name: $name, description: $description, status: $status, crew_id: $crewID, object_id: $objectID}) {
       returning {
         id
       }
@@ -23,17 +20,17 @@ const createEventQuery = `
 
 const getObjectsQuery = `
   query getObjects {
-    objects {
+    object {
       address
-      Id
+      id
     }
   }
 `;
 
 const getCrewsQuery = `
   query getCrews {
-    crews {
-      Id
+    crew {
+      id
       abbreviation
     }
   }
@@ -42,33 +39,10 @@ const getCrewsQuery = `
 export const ERROR = {
   UNHANDLED: t("error.newEvent.eventServiceOffline"),
   SERVICE_DOWN: t("error.newEvent.dataIsInvalid"),
-  INVALID: t("error.newEvent.networkError"),
-  UNEXPECTED_OK_RESPONSE: t("error.newEvent.unexpectedResponse"),
   NETWORK_FAILED: t("error.newEvent.unhandledState"),
 };
 
 const responseHandling = {
-  invalidCreds: {
-    check: pipe(
-      getPath(["errors", 0, "message"]),
-      chain(safe(isString)),
-      chain(safe(m => m.match(/not.*valid.*response.*hook/im))),
-      map(lengthGt(0)),
-      option(false),
-    ),
-    output: () => Rejected(ERROR.INVALID),
-  },
-  validCreds: {
-    check: pipe(
-      getPath(["data", "login"]),
-      map(hasProps(["token", "refreshToken", "user"])),
-      option(false),
-    ),
-    output: maybeToAsync(
-      ERROR.UNEXPECTED_OK_RESPONSE,
-      getPath(["data", "login"]),
-    ),
-  },
   serviceDown: {
     check: pipe(
       getPath(["errors", 0, "message"]),
@@ -92,50 +66,32 @@ const responseHandling = {
   onUnknownState: pipe(tap(console.error), () => Rejected(ERROR.UNHANDLED)),
 };
 
-export const asyncGetCrews = (secret, token) => {
+export const asyncGetCrews = token => {
   return fetchGql(
     env.API_ENDPOINT,
-    {
-      "x-hasura-admin-secret": secret,
-      "authorization": token
-    },
+    {"x-hasura-admin-secret": env.API_SECRET, "authorization": token},
     getCrewsQuery,
     null
   );
 };
 
-export const asyncGetObjects = (secret, token) => {
+export const asyncGetObjects = token => {
   return fetchGql(
     env.API_ENDPOINT,
-    {
-      "x-hasura-admin-secret": secret,
-      "authorization": token
-    },
+    {"x-hasura-admin-secret": env.API_SECRET, "authorization": token},
     getObjectsQuery,
     null
   );
 };
 
-export const asyncCreateEvent = ({secret, token, name, desctiption, status, crewID, objectID}) => {
+export const asyncCreateEvent = ({token, name, description, status, crewID, objectID}) => {
   return fetchGql(
     env.API_ENDPOINT,
-    {"x-hasura-admin-secret": secret, "authorization": token},
+    {"x-hasura-admin-secret": env.API_SECRET, "authorization": token},
     createEventQuery,
-    {name, desctiption, status, crewID, objectID}
+    {name, description, status, crewID, objectID}
   ).bichain(
-    caseMap(responseHandling.onUnknownState, [
-      [
-        responseHandling.networkError.check,
-        responseHandling.networkError.output,
-      ],
-    ]),
-    caseMap(responseHandling.onUnknownState, [
-      [responseHandling.serviceDown.check, responseHandling.serviceDown.output],
-      [
-        responseHandling.invalidCreds.check,
-        responseHandling.invalidCreds.output,
-      ],
-      [responseHandling.validCreds.check, responseHandling.validCreds.output],
-    ]),
+    caseMap(responseHandling.onUnknownState,[[responseHandling.networkError.check, responseHandling.networkError.output]]),
+    caseMap(responseHandling.onUnknownState, [[responseHandling.serviceDown.check, responseHandling.serviceDown.output]]),
   );
 };
