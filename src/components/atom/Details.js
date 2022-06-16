9690c28f740deafc6bd7c402c13d508db1012fa2
Alexander Kremeshnoi <alexander.kremeshnoi@gmail.com>
2022-06-16 19:19:18 +0300

    chore: clean up Details

--- src/components/atom/Details.js
+++ src/components/atom/Details.js
@@ -1,19 +1,19 @@
-import React from "react";
+import React from 'react';
 
-import useBoolean from "../../hook/useBoolean";
-import {withMergedClassName} from "../../util/react";
-import {ChevronDownIcon, ChevronUpIcon} from "@heroicons/react/outline";
+import useBoolean from '../../hook/useBoolean';
+import {withMergedClassName} from '../../util/react';
+import {ChevronDownIcon, ChevronUpIcon} from '@heroicons/react/outline';
 
 const Details = withMergedClassName(
-  "flex flex-shrink-1",
-  ({Tag =  "div", title, children}) => {
+  'flex flex-shrink-1',
+  ({Tag =  'div', title, children}) => {
     const [isShown, setShown] = useBoolean(false);
     return (
       <Tag>
-        <div className={`w-full overflow-y-auto scrollbar-gone text-slate-400 ${isShown ? "h-64" : "h-auto"}`}>
-          <div className="flex w-full sticky top-0 flex-row items-center justify-between border-t py-2 px-6 bg-gray-100">
-            <button onClick={setShown} className="text-sm">{title}</button>
-            <button onClick={setShown} className="flex items-center h-2 w-4">
+        <div className={`w-full overflow-y-auto scrollbar-gone text-slate-400 ${isShown ? 'h-64' : 'h-auto'}`}>
+          <div className='flex w-full sticky top-0 flex-row items-center justify-between border-t py-2 px-6 bg-gray-100'>
+            <button onClick={setShown} className='text-sm'>{title}</button>
+            <button onClick={setShown} className='flex items-center h-2 w-4'>
               {isShown
                 ? <ChevronDownIcon/>
                 : <ChevronUpIcon/>
