a2f6d7e7d7c635cb939435ac3132e6d3e5e6ec00
Alexander Kremeshnoi <alexander.kremeshnoi@gmail.com>
2022-06-16 19:09:40 +0300

    chore: cleaned up App.js

--- src/App.js
+++ src/App.js
@@ -1,40 +1,40 @@
-import React, { useContext } from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { AuthProvider } from "./context/authContext";
-import GlobalContext from "./context/globalContext";
-import ProtectedRoute from "./feature/protected";
-import { GraphQLClient, ClientContext } from "graphql-hooks";
-import memCache from "graphql-hooks-memcache";
-import { QueryClient, QueryClientProvider, useQuery } from "react-query";
-import { SubscriptionClient } from "subscriptions-transport-ws";
+import React, { useContext } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { AuthProvider } from './context/authContext';
+import GlobalContext from './context/globalContext';
+import ProtectedRoute from './feature/protected';
+import { GraphQLClient, ClientContext } from 'graphql-hooks';
+import memCache from 'graphql-hooks-memcache';
+import { QueryClient, QueryClientProvider, useQuery } from 'react-query';
+import { SubscriptionClient } from 'subscriptions-transport-ws';
 
-import Login from "./layout/login";
-import Object from "./layout/object/object";
-import Keys from "./layout/key/keys";
-import Tasks from "./layout/tasks";
-import Drivers from "./layout/driver/drivers";
-import Clients from "./layout/client/clients";
-import Objects from "./layout/object/objects";
-import Modems from "./layout/modem/modems";
-import Create from "./layout/create";
-import Driver from "./layout/driver/driver";
-import Client from "./layout/client/client";
-import Key from "./layout/key/key";
-import Modem from "./layout/modem/modem";
-import New from "./layout/new";
-import Crews from "./layout/crew/crews";
-import CreateCrew from "./layout/crew/createCrew";
-import Permission from "./layout/permission/permissions";
-import Breach from "./layout/breach/breach";
-import Breaches from "./layout/breach/breaches";
-import Dislocations from "./layout/dislocation/dislocations";
-import Dislocation from "./layout/dislocation/dislocation";
-import PermissionConfirmation from "./layout/permission/permissionConfirmation";
+import Login from './layout/login';
+import Object from './layout/object/object';
+import Keys from './layout/key/keys';
+import Tasks from './layout/tasks';
+import Drivers from './layout/driver/drivers';
+import Clients from './layout/client/clients';
+import Objects from './layout/object/objects';
+import Modems from './layout/modem/modems';
+import Create from './layout/create';
+import Driver from './layout/driver/driver';
+import Client from './layout/client/client';
+import Key from './layout/key/key';
+import Modem from './layout/modem/modem';
+import New from './layout/new';
+import Crews from './layout/crew/crews';
+import CreateCrew from './layout/crew/createCrew';
+import Permission from './layout/permission/permissions';
+import Breach from './feature/breach/layout/BreachLayout';
+import Breaches from './feature/breach/layout/breaches';
+import Dislocations from './layout/dislocation/dislocations';
+import Dislocation from './layout/dislocation/dislocation';
+import PermissionConfirmation from './layout/permission/permissionConfirmation';
 
-import NewTaskLayout from "./feature/task/layout/NewTaskLayout";
+import NewTaskLayout from './feature/task/layout/NewTaskLayout';
 import DashboardLayout from './feature/dashboard/layout/DashboardLayout';
 
-import NotFound from "./layout/notFound";
+import NotFound from './layout/notFound';
 import GoogleContextProvider from './context/googleApiContext';
 
 const queryClient = new QueryClient();
@@ -42,22 +42,22 @@ const queryClient = new QueryClient();
 function App() {
   const { globalToken, setGlobalToken } = useContext(GlobalContext);
   const client = new GraphQLClient({
-    url: "https://ec.swarm.testavimui.eu/v1/graphql",
+    url: 'https://ec.swarm.testavimui.eu/v1/graphql',
     headers: {
-      "x-hasura-admin-secret": "secret",
+      'x-hasura-admin-secret': 'secret',
     },
     cache: memCache(),
     subscriptionClient: new SubscriptionClient(
-      "ws://ec.swarm.testavimui.eu/v1/graphql",
+      'ws://ec.swarm.testavimui.eu/v1/graphql',
       {
         reconnect: true,
         lazy: true,
         inactivityTimeout: 30000,
         connectionParams: {
           headers: {
-            "content-type": "application/json",
-            "x-hasura-admin-secret": "secret",
-            Authorization: globalToken ? "Bearer" + String(globalToken) : "",
+            'content-type': 'application/json',
+            'x-hasura-admin-secret': 'secret',
+            Authorization: globalToken ? 'Bearer' + String(globalToken) : '',
           },
         },
       }
@@ -72,31 +72,31 @@ function App() {
             <GoogleContextProvider>
               <Routes>
                 <Route element={<ProtectedRoute />}>
-                  <Route path="/" exec element={<Login />} />
-                  <Route path="Dashboard" element={<DashboardLayout />} />
-                  <Route path="Keys" element={<Keys />} />
-                  <Route path="Tasks" element={<Tasks />} />
-                  <Route path="NewTask" element={<NewTaskLayout />} />
-                  <Route path="Drivers" element={<Drivers />} />
-                  <Route path="Clients" element={<Clients />} />
-                  <Route path="Objects" element={<Objects />} />
-                  <Route path="Modems" element={<Modems />} />
-                  <Route path="Create" element={<Create />} />
-                  <Route path="New" element={<New />} />
-                  <Route path="Driver/:id" element={<Driver />} />
-                  <Route path="Client/:id" element={<Client />} />
-                  <Route path="Modem/:id" element={<Modem />} />
-                  <Route path="Key/:id" element={<Key />} />
-                  <Route path="Object/:id" element={<Object />} />
-                  <Route path="Dislocation/:id" element={<Dislocation />} />
-                  <Route path="CreateCrew/:id" element={<CreateCrew />} />
-                  <Route path="Crews" element={<Crews />} />
-                  <Route path="Permissions" element={<Permission />} />
-                  <Route path="PermissionConfirmation" element={<PermissionConfirmation />} />
-                  <Route path="Breaches" element={<Breaches />} />
-                  <Route path="Dislocations" element={<Dislocations />} />
-                  <Route path="Breach" element={<Breach />} />
-                  <Route path="*" element={<NotFound />} />
+                  <Route path='/' exec element={<Login />} />
+                  <Route path='Dashboard' element={<DashboardLayout />} />
+                  <Route path='Keys' element={<Keys />} />
+                  <Route path='Tasks' element={<Tasks />} />
+                  <Route path='NewTask' element={<NewTaskLayout />} />
+                  <Route path='Drivers' element={<Drivers />} />
+                  <Route path='Clients' element={<Clients />} />
+                  <Route path='Objects' element={<Objects />} />
+                  <Route path='Modems' element={<Modems />} />
+                  <Route path='Create' element={<Create />} />
+                  <Route path='New' element={<New />} />
+                  <Route path='Driver/:id' element={<Driver />} />
+                  <Route path='Client/:id' element={<Client />} />
+                  <Route path='Modem/:id' element={<Modem />} />
+                  <Route path='Key/:id' element={<Key />} />
+                  <Route path='Object/:id' element={<Object />} />
+                  <Route path='Dislocation/:id' element={<Dislocation />} />
+                  <Route path='CreateCrew/:id' element={<CreateCrew />} />
+                  <Route path='Crews' element={<Crews />} />
+                  <Route path='Permissions' element={<Permission />} />
+                  <Route path='PermissionConfirmation' element={<PermissionConfirmation />} />
+                  <Route path='Breaches' element={<Breaches />} />
+                  <Route path='Dislocations' element={<Dislocations />} />
+                  <Route path='Breach' element={<Breach />} />
+                  <Route path='*' element={<NotFound />} />
                 </Route>
               </Routes>
             </GoogleContextProvider>
@@ -105,23 +105,6 @@ function App() {
       </QueryClientProvider>
     </Router>
   );
-
-  // return (
-  //   <Router>
-  //     <TableComponent>
-  //       <TableComponent.Head>
-  //         <tr>
-  //           <TableComponent.Header>Hello</TableComponent.Header>
-  //         </tr>
-  //       </TableComponent.Head>
-  //       <TableComponent.Body>
-  //         <TableComponent.Row>
-  //           <TableComponent.Data>Hello as well</TableComponent.Data>
-  //         </TableComponent.Row>
-  //       </TableComponent.Body>
-  //     </TableComponent>
-  //   </Router>
-  // );
-}
+};
 
 export default App;
