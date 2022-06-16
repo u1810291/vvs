d968c951d4c50558eaac8bb72f1dc547337ed0df
Alexander Kremeshnoi <alexander.kremeshnoi@gmail.com>
2022-06-16 19:07:13 +0300

    feat: add new rule to eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -17,6 +17,7 @@ module.exports = {
     'rules': {
         'react/prop-types': 0,
         'quotes': ['error', 'single'],
-        'jsx-quotes': ['error', 'prefer-single']
+        'react/react-in-jsx-scope': 'off',
+        'jsx-quotes': ['error', 'prefer-single'],
     }
 }
