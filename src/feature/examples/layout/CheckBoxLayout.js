62c4f993499eb74b6ca4a6e8b51820d11426425f
Alexander Kremeshnoi <alexander.kremeshnoi@gmail.com>
2022-06-17 13:02:12 +0300

    feat(WIP): create CheckBox

---
+++ src/feature/examples/layout/CheckBoxLayout.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import CheckBox from '../../../components/atom/input/CheckBox';
+
+const ComboBoxLayout = () => {
+  return (
+    <section className={'ml-auto mr-auto w-1/2 mt-40'}>
+      <CheckBox/>
+    </section>
+  );
+};
+
+export default ComboBoxLayout;
