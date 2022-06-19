90410b4a3560ceaec7c8a514643c58cffc3cc32b
Lukas Levickas <lukas.l@s-e.lt>
2022-06-19 09:31:13 +0300

    fix: env var name

--- src/env.js
+++ src/env.js
@@ -1,6 +1,6 @@
 const ENV = {
   API_ENDPOINT: process.env.REACT_APP_BACKEND_URL,
-  GOOGLE_MAPS_API_KEY: process.env.GOOGLE_MAPS_API_KEY,
+  GOOGLE_MAPS_API_KEY: process.env.REACT_APP_GOOGLE_MAP_API_KEY,
   IS_DEV: ['true', 1, '1', 't', 'yes', 'y'].includes(process.env.REACT_APP_IS_DEV.toLowerCase()),
   API_SECRET: process.env.REACT_APP_BACKEND_SECRET,
 };
