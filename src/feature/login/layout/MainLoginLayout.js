0d6ad49ff321aac7774e8c3561f8882a4e1b6729
Lukas Levickas <lukas.l@s-e.lt>
2022-06-20 15:49:56 +0300

    fix: login layout bg size

--- src/feature/login/layout/MainLoginLayout.js
+++ src/feature/login/layout/MainLoginLayout.js
@@ -4,7 +4,7 @@ const MainLoginLayout = ({
   title = 'Sign in to your account',
   ...props
 }) => (
-  <div className='h-full bg-gray-50 dark:bg-gray-900'>
+  <div className='min-h-screen h-full bg-gray-50 dark:bg-gray-900'>
     <div className='min-h-full flex flex-col justify-center py-12 sm:px-6 lg:px-8'>
       <div className='sm:mx-auto sm:w-full sm:max-w-md'>
         <img className='mx-auto h-12 w-auto' src='https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg' alt='Workflow' />
