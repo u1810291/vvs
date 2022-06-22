71d780086b36718ab12360a0ed3935cf5bdc4f46
Alexander Kremeshnoi <alexander.kremeshnoi@gmail.com>
2022-06-22 19:03:39 +0300

    feat: remove unused imports rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,13 +12,15 @@ module.exports = {
         'sourceType': 'module'
     },
     'plugins': [
-        'react'
+        'react',
+        'unused-imports'
     ],
     'rules': {
         'react/prop-types': 0,
         'quotes': ['error', 'single'],
         'react/react-in-jsx-scope': 'off',
         'jsx-quotes': ['error', 'prefer-single'],
-        'object-curly-spacing': ['error', 'never']
+        'object-curly-spacing': ['error', 'never'],
+        'unused-imports/no-unused-imports': 'error'
     }
 }
