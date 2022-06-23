dbcdff56caf14028c5132d545044d45e41b97f89
Alexander Kremeshnoi <alexander.kremeshnoi@gmail.com>
2022-06-23 19:12:09 +0300

    fix: valid template nesting

--- src/components/Filter/Dropdown.js
+++ src/components/Filter/Dropdown.js
@@ -11,11 +11,11 @@ const Dropdown = withComponentFactory(Menu, {
     const {t} = useLanguage();
     return (
       <HeadlessMenu.Button className='inline-flex justify-center w-full px-4 py-1 text-sm font-medium bg-white focus:outline-none hover:bg-gray-50 rounded-md hover:text-gray-900 text-gray-500' {...props}>
-      <svg className='mr-2' width='15' height='16' viewBox='0 0 15 16' fill='none' xmlns='http://www.w3.org/2000/svg'>
-      <path d='M7.07057 15.0703V0.928943' stroke='#D7DEE6' strokeWidth='3'/>
-      <path d='M-0.000134685 8.00038L14.1412 8.00038' stroke='#D7DEE6' strokeWidth='3'/>
-      </svg>
-      {t`Filter.Button.addColumn`}
+        <svg className='mr-2' width='15' height='16' viewBox='0 0 15 16' fill='none' xmlns='http://www.w3.org/2000/svg'>
+          <path d='M7.07057 15.0703V0.928943' stroke='#D7DEE6' strokeWidth='3'/>
+          <path d='M-0.000134685 8.00038L14.1412 8.00038' stroke='#D7DEE6' strokeWidth='3'/>
+        </svg>
+        {t`Filter.Button.addColumn`}
       </HeadlessMenu.Button>
     )
   },
