5a5034db714172d9f9c9add1b427fd388d829d57
Alexander Kremeshnoi <alexander.kremeshnoi@gmail.com>
2022-06-23 22:38:18 +0300

    chore: clean up Routes.js

--- src/Routes.js
+++ src/Routes.js
@@ -1,59 +1,49 @@
 import React from 'react';
 import {Route} from 'react-router-dom';
 
-import TaskRoute from './feature/task/routes';
-import CrewRoute from './feature/crew/routes';
+import TaskFeature from './feature/task/routes';
+import CrewFeature from './feature/crew/routes';
 import LoginFeature from 'feature/login/routes';
 import ObjectFeature from './feature/object/routes';
 import DashboardFeature from './feature/dashboard/routes';
+import PermissionFeature from './feature/permission/routes';
 
 import NotFoundLayout from './layout/NotFoundLayout';
 
 const Routes = (
   <>
-    {TaskRoute}
-    {CrewRoute}
+    {TaskFeature}
+    {CrewFeature}
     {LoginFeature}
     {ObjectFeature}
     {DashboardFeature}
+    {PermissionFeature}
     <Route isHidden path='*' element={<NotFoundLayout />} />
   {
     /**
      * TODO: redo like LoginFeature, like ObjectFeature
      *
       import Keys from './layout/key/keys';
-      import TaskListLayout from './layout/tasks';
       import Drivers from './layout/driver/drivers';
       import Clients from './layout/client/clients';
       import Modems from './layout/modem/modems';
-      import Create from './layout/create';
       import Driver from './layout/driver/driver';
       import Client from './layout/client/client';
       import Key from './layout/key/key';
       import Modem from './layout/modem/modem';
-      import New from './layout/new';
-      import Permission from './layout/permission/permissions';
       import Breach from './feature/breach/layout/BreachLayout';
       import Breaches from './feature/breach/layout/breaches';
       import Dislocations from './layout/dislocation/dislocations';
       import Dislocation from './layout/dislocation/dislocation';
-      import PermissionConfirmation from './layout/permission/permissionConfirmation';
-      import TaskEditLayout from './feature/task/layout/TaskEditLayout';
       <Route path='keys' element={<Keys />} />
-      <Route path='tasks' element={<TaskListLayout />} />
-      <Route path='new-task' element={<TaskEditLayout />} />
       <Route path='drivers' element={<Drivers />} />
       <Route path='clients' element={<Clients />} />
       <Route path='modems' element={<Modems />} />
-      <Route path='create' element={<Create />} />
-      <Route path='new' element={<New />} />
       <Route path='driver/:id' element={<Driver />} />
       <Route path='client/:id' element={<Client />} />
       <Route path='modem/:id' element={<Modem />} />
       <Route path='key/:id' element={<Key />} />
       <Route path='dislocation/:id' element={<Dislocation />} />
-      <Route path='permissions' element={<Permission />} />
-      <Route path='permission-confirmation' element={<PermissionConfirmation />} />
       <Route path='dislocations' element={<Dislocations />} />
       <Route path='breach' element={<Breach />} />
       <Route path='breaches' element={<Breaches />} />
