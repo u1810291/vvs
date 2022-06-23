49d34a2d7df338596391af24d85769994437de3d
Alexander Kremeshnoi <alexander.kremeshnoi@gmail.com>
2022-06-23 20:28:23 +0300

    chore: clean up

--- src/context/route.js
+++ src/context/route.js
@@ -1,55 +1,23 @@
+import React, {createContext, useMemo, useReducer} from 'react';
 
-const Contex = createContext();
+// FIXME: This is probably an obsolete context provider
 
-const ContexProvider = memo(({children, ...props}) => {
+const Context = createContext();
+
+const ContexProvider = useMemo(({children, ...props}) => {
   const [routes, setRoutes] = useReducer((older, newer) => ({...older, ...newer}), {});
 
-
-
   return (
-    <Contex.Provider value={{routes, setRoutes}}>
+    <Context.Provider value={{routes, setRoutes}}>
       {children}
-    </Contex.Provider>
+    </Context.Provider>
   );
 }, () => true);
 
 const useRoute = () => {
   const context = useContext(Contex);
-  if (context === undefined) throw new Error('useSetup must be used within a ContexProvider');
+  if (context === undefined) throw new Error('useRoute must be used within a ContextProvider');
   return context;
-}
+};
 
-export {
-  ContexProvider,
-  useRoute,
-}
-
-            // <Routes>
-            //   <Route element={<ProtectedRoute />}>
-            //     <Route path='/' exec element={<Login />} />
-            //     <Route path='Dashboard' element={<Dashboard />} />
-            //     <Route path='Keys' element={<Keys />} />
-            //     <Route path='Tasks' element={<Tasks />} />
-            //     <Route path='NewTask' element={<NewTaskLayout />} />
-            //     <Route path='Drivers' element={<Drivers />} />
-            //     <Route path='Clients' element={<Clients />} />
-            //     <Route path='Objects' element={<Objects />} />
-            //     <Route path='Modems' element={<Modems />} />
-            //     <Route path='Create' element={<Create />} />
-            //     <Route path='New' element={<New />} />
-            //     <Route path='Driver/:id' element={<Driver />} />
-            //     <Route path='Client/:id' element={<Client />} />
-            //     <Route path='Modem/:id' element={<Modem />} />
-            //     <Route path='Key/:id' element={<Key />} />
-            //     <Route path='Object/:id' element={<Object />} />
-            //     <Route path='Dislocation/:id' element={<Dislocations />} />
-            //     <Route path='Dislocation/' element={<Dislocations />} />
-            //     <Route path='CrewEditLayout/:id' element={<CrewEditLayout />} />
-            //     <Route path='Crews' element={<Crews />} />
-            //     <Route path='Permissions' element={<Permission />} />
-            //     <Route path='PermissionConfirmation' element={<PermissionConfirmation />} />
-            //     <Route path='Breaches' element={<Breaches />} />
-            //     <Route path='Breach' element={<Breach />} />
-            //     <Route path='*' element={<NotFound />} />
-            //   </Route>
-            // </Routes>
+export {ContexProvider, useRoute};
