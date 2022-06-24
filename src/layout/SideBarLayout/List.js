df5763560485df4a32d75130b339e113287a96a9
Alexander Kremeshnoi <alexander.kremeshnoi@gmail.com>
2022-06-24 21:31:33 +0300

    feat: play around styles for SideBar Menu

--- src/layout/SideBarLayout/List.js
+++ src/layout/SideBarLayout/List.js
@@ -17,7 +17,7 @@ const parseRoutes = (t, component) => renderChildren((c, index) => {
     <Fragment key={String(`${c?.props?.path}-${index}-${c?.props?.children?.length}`)}>
       {isRoute && (
         <NavLink
-          className={({isActive}) => `group flex items-center px-2 py-2 text-sm font-medium rounded-md ${isActive ? 'bg-gray-900 bg-opacity-10 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white'}`}
+          className={({isActive}) => `group flex items-center px-2 py-2 mb-6 last-of-type:mb-0 text-base font-medium rounded-md ${isActive ? 'bg-gray-900 bg-opacity-10 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white'}`}
           to={c.props.path}
         >
         {t(c?.props?.translationKey, {ns: c?.props?.translationNs}) || c?.props?.path}
