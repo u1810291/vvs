cb13f20406b9ae793b9d52ddfd26016180966823
Alexander Kremeshnoi <alexander.kremeshnoi@gmail.com>
2022-06-24 21:30:35 +0300

    feat: add lt translations to crew listing feature

--- src/feature/crew/routes.js
+++ src/feature/crew/routes.js
@@ -1,9 +1,12 @@
 import {lazy} from 'react';
 import i18next from 'i18next';
-import EN from './i18n/en.json';
 import {getExactHiddenRoute, getExactRoute} from 'util/react';
 
+import EN from './i18n/en.json';
+import LT from './i18n/lt.json'
+
 i18next.addResourceBundle('en', 'crew', EN);
+i18next.addResourceBundle('lt', 'crew', LT);
 
 export const CrewEditRoute = getExactHiddenRoute('crew', 'menu.edit', '/crew/:id', lazy(() => import('./layout/CrewEditLayout')), null);
 export const CrewListRoute = getExactRoute('crew', 'menu.list', '/crew', lazy(() => import('./layout/CrewListLayout')), null);
