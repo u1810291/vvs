08aa438f1b87d59cf4fa082400822b388f5c6ab4
Alexander Kremeshnoi <alexander.kremeshnoi@gmail.com>
2022-06-24 21:27:51 +0300

    feat: add lt translations to dashboard feature

--- src/feature/dashboard/routes.js
+++ src/feature/dashboard/routes.js
@@ -1,11 +1,14 @@
-import i18next from 'i18next';
 import {lazy} from 'react';
+import i18next from 'i18next';
 import {getExactRoute} from 'util/react';
+
 import EN from './i18n/en.json';
+import LT from './i18n/lt.json';
 
 export const ROOT_PAGE = '/';
 
 i18next.addResourceBundle('en', 'dashboard', EN);
+i18next.addResourceBundle('lt', 'dashboard', LT);
 
 const DashboardRoute = getExactRoute(
   'dashboard',
