f6440e888d905a6d075c89fab1bcc4221a2a1ed2
Alexander Kremeshnoi <alexander.kremeshnoi@gmail.com>
2022-07-22 14:44:09 +0300

    feat: extend Card.js with Xs property

--- src/components/atom/Card.js
+++ src/components/atom/Card.js
@@ -7,6 +7,7 @@ const CLASS_NAME = 'bg-white text-black dark:bg-zinc-900 shadow bg-opacity-80 da
 
 const Card = withMergedClassName(`${CLASS_NAME} py-8 px-4 sm:px-10`, div);
 
+Card.Xs = withMergedClassName(`${CLASS_NAME}`, div)
 Card.Sm = withMergedClassName(`${CLASS_NAME} py-4 px-2`, div);
 Card.SmRounded = withMergedClassName(`${CLASS_NAME} py-4 px-2 sm:rounded-lg`, div);
 
