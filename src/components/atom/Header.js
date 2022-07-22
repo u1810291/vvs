2a0997c659c83d49998ec65e64e8f33dc9db30e0
Alexander Kremeshnoi <alexander.kremeshnoi@gmail.com>
2022-07-22 14:44:45 +0300

    feat(WIP): set fixed height for header

--- src/components/atom/Header.js
+++ src/components/atom/Header.js
@@ -5,7 +5,7 @@ import useLanguage from '../../hook/useLanguage';
 const Header = ({className = 'flex items-center justify-between p-5 border-b border-gray-300', children, ...props}) => {
   const {t} = useLanguage();
   return (
-    <header className={`${className}`} {...props}>
+    <header className={`${className} h-20`} {...props}>
       {children}
     </header>
   );
