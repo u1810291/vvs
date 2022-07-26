19214ff6ee0e3bc379cbfa666833d47a741e4fdf
Alexander Kremeshnoi <alexander.kremeshnoi@gmail.com>
2022-07-26 10:04:23 +0300

    feat: extend Map component with fitBounds logic

--- src/feature/map/component/Map.js
+++ src/feature/map/component/Map.js
@@ -1,33 +1,34 @@
-import React from 'react';
+import React, {memo, useEffect} from 'react';
+import {compareMemo} from 'util/react';
 import {GoogleMap} from '@react-google-maps/api';
-import {useGoogleApiContext} from '../../../context/google';
+import {useGoogleApiContext} from 'context/google';
 
-const mapContainerStyle = {
-  width: '100%',
-  height: '100%',
-};
-
-const mapCenter = {
-  lat: 55.95,
-  lng: 23.33,
-};
-
-const Map = ({singleMarkerCoords, children}) => {
-  const {isLoaded, onMapLoad, onMapUnmount} = useGoogleApiContext();
+const Map = memo(({id, zoom, coods, children}) => {
+  const {bounds, googleMap, isLoaded, onMapLoad, onMapUnmount} = useGoogleApiContext();
   if (!isLoaded) return null;
+  useEffect(() => {
+    googleMap?.setCenter({
+      lat: bounds?.getCenter().lat(),
+      lng: bounds?.getCenter().lng(),
+    })
+  }, [googleMap, coods])
   return (
     <div className='w-full h-full relative'>
       <GoogleMap
-        zoom={14}
-        center={singleMarkerCoords || mapCenter}
+        id={id}
         onLoad={onMapLoad}
         onUnmount={onMapUnmount}
-        mapContainerStyle={mapContainerStyle}
+        mapContainerStyle={{
+          width: '100%',
+          height: '100%',
+        }}
       >
         {children}
       </GoogleMap>
     </div>
   );
-};
+}, compareMemo(['id'], ['coodss'], ['bounds'], ['routes']));
+
+Map.displayName = 'Map';
 
 export default Map;
