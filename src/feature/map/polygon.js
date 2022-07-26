c248bb84c93d931dc643e7f7945d2f0136908b5f
Alexander Kremeshnoi <alexander.kremeshnoi@gmail.com>
2022-07-26 10:04:51 +0300

    feat(WIP): create polygon constants

---
+++ src/feature/map/polygon.js
@@ -0,0 +1,17 @@
+export const POLYGON_OPTIONS_EDIT = {
+  strokeOpacity: 1,
+  fillOpacity: 0.4,
+  strokeWeight: 0.8,
+  fillColor: '#C32A2F',
+  strokeColor: '#C32A2F',
+  click: true,
+  draggable: true
+};
+
+export const POLYGON_OPTIONS_DISPLAY = {
+  strokeOpacity: 1,
+  fillOpacity: 0.4,
+  strokeWeight: 0.8,
+  fillColor: '#C32A2F',
+  strokeColor: '#C32A2F',
+};
