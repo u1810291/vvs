c83ed046365519a288885b40bf66cda69823d3b7
hi-sanchoman <hi.sanchoman@gmail.com>
2022-08-05 12:46:14 +0600

    fix: filter components inner margin

--- src/components/atom/input/SelectBox/Button.js
+++ src/components/atom/input/SelectBox/Button.js
@@ -5,7 +5,7 @@ import Nullable from 'components/atom/Nullable';
 
 const Button = ({displayName, placeholder, props}) => {
   return <Listbox.Button
-    className='bg-white h-[32px] relative w-full border border-gray-300 rounded-md shadow-sm pl-3 pr-10 py-2 text-left cursor-default focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm' {...props}>
+    className='bg-white h-[32px] relative w-full border border-gray-300 rounded-md shadow-sm pl-3 pr-10 py-1 text-left cursor-default focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm' {...props}>
     <span className='block truncate'>{displayName}</span>
 
     <Nullable on={placeholder && !displayName}>
