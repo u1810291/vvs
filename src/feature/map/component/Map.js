0a96699ba9222e84f158d01fbf6092305bda560d
Alexander Kremeshnoi <alexander.kremeshnoi@gmail.com>
2022-08-16 21:44:04 +0300

    fix: fixed map zooming

--- src/feature/map/component/Map.js
+++ src/feature/map/component/Map.js
@@ -23,11 +23,16 @@ const Map = memo(({id, zoom, coordinates, path, children}) => {
   if (!isLoaded) return null;
   useEffect(() => {
     if (googleMap && bounds) {
+      // TODO: Fix polygon shapes
       if (coordinates?.length) map(coordinate => bounds.extend(coordinate), coordinates);
       else if (!coordinates?.length) bounds.extend(LITHUANIA_COORDINATES);
-      googleMap.setZoom(zoom || 7);
       setBounds(new google.maps.LatLngBounds());
       googleMap.setCenter({lat: bounds.getCenter().lat(), lng: bounds.getCenter().lng()});
+      const listener = new google.maps.event.addListener(googleMap, 'idle',() => {
+        if (zoom) googleMap.setZoom(zoom)
+        else if (!zoom && googleMap.getZoom() > 7) googleMap.setZoom(7);
+        new google.maps.event.removeListener(listener);
+      });
     }
   }, [googleMap, coordinates, zoom]);
 
