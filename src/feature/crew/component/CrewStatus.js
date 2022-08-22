09e9782eb7a042869cace7d16af8ede9ce7d2318
Alexander Kremeshnoi <alexander.kremeshnoi@gmail.com>
2022-08-22 20:52:27 +0300

    feat: change styles

--- src/feature/crew/component/CrewStatus.js
+++ src/feature/crew/component/CrewStatus.js
@@ -16,15 +16,15 @@ const is = propEq('status');
 const CrewStatusBase = (props) => {
   const {t} = useTranslation('crew', {keyPrefix: 'list.status'});
   return (
-    <div className={'flex flex-col items-center justify-center rounded-md bg-white min-w-20 w-fit p-1 text-white text-xs font-normal'} {...props}>
+    <div className={'flex flex-col items-center justify-center rounded-md min-w-20 w-fit p-1 text-white text-xs font-normal'} {...props}>
       {t(`${pipe(getProp('status'), chain(safe(isString)), option('-'))(props)}`)}
     </div>
   );
 };
 
-const CLASS_NAME = 'flex flex-col items-start justify-center rounded-md bg-white min-w-20 w-fit p-1 text-white text-xs font-normal';
+const CLASS_NAME = 'flex flex-col items-start justify-center rounded-md min-w-20 w-fit p-1 text-white text-xs font-normal';
 
-const CrewStatus = withMergedClassName(`${CLASS_NAME} bg-white text-black`, CrewStatusBase);
+const CrewStatus = withMergedClassName(`${CLASS_NAME} text-black`, CrewStatusBase);
 CrewStatus.Busy = withMergedClassName(`${CLASS_NAME} bg-burgundy`, CrewStatusBase);
 CrewStatus.Break = withMergedClassName(`${CLASS_NAME} bg-tango`, CrewStatusBase);
 CrewStatus.Ready = withMergedClassName(`${CLASS_NAME} bg-mantis`, CrewStatusBase);
